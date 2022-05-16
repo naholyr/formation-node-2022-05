819703552dbc149db1ebdf258a5c0ca2de5890d7
Nicolas Chambrier <naholyr@gmail.com>
2022-05-16 21:39:38 +0200

    Config with dotenv

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import { app } from "./src/app";
 
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("Server ready http://localhost:3001");
+  console.log(`Server ready http://localhost:${process.env.PORT}`);
 });
