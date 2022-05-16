397ec954800ae081a3209d19c3bfb93048ce8fb5
Nicolas Chambrier <naholyr@gmail.com>
2022-05-16 18:55:45 +0200

    Express handlers

--- app/src/app.ts
+++ app/src/app.ts
@@ -3,3 +3,22 @@ import { fibo } from "./fibo";
 import type { Handler } from "express";
 
 export const app = express();
+
+const log: Handler = (req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log(new Date(), req.method, req.url);
+  // Call next or chain is broken!
+  next();
+};
+
+app.get("/*", log);
+
+// Simplest GET handler
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+
+// Route parameters
+app.get("/fibo/:nb([0-9]+)", (req, res) => {
+  res.send({ result: fibo(Number(req.params.nb)) });
+});
